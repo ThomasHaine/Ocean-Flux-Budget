efbecec710d685402b75460159902641a7c7a3f3
thaine1 <Thomas.Haine@jhu.edu>
2023-04-09 13:49:47 -0400

    Add correlation table

--- UpdateStraits.m
+++ UpdateStraits.m
@@ -8,6 +8,11 @@ for ss = 1:numel(straitNames)
     straits.(strait).salt_flux = salt_flux ;
     straits.(strait).heat_flux = heat_flux ;
     straits.(strait).LFC_flux  =  LFC_flux ;
+
+    [salt_flux_correl,heat_flux_correl,LFC_flux_correl] = ComputeCorrelations(straits.(strait)) ;
+    straits.(strait).salt_flux_correl = salt_flux_correl ;
+    straits.(strait).heat_flux_correl = heat_flux_correl ;
+    straits.(strait).LFC_flux_correl  =  LFC_flux_correl ;
 end % ss
 end
 
@@ -18,4 +23,26 @@ mass_flux =                   strait.density .* vol_flux ;
 heat_flux = FluxParams.C_p .* strait.density .* vol_flux .* (strait.temperature - FluxParams.T_ref) ;
 LFC_flux  =                                     vol_flux .* (strait.salinity    - FluxParams.S_ref) ;
 salt_flux =                   strait.density .* vol_flux .*  strait.salinity ;
+end
+
+function [salt_flux_correl,heat_flux_correl,LFC_flux_correl] = ComputeCorrelations(strait)
+
+[r,p] = corrcoef([strait.salt_flux strait.vol_flux strait.salinity]) ;
+salt_flux_correl.vol_flux.r = r(1,2) ;
+salt_flux_correl.vol_flux.p = p(1,2) ;
+salt_flux_correl.salinity.r = r(1,3) ;
+salt_flux_correl.salinity.p = p(1,3) ;
+
+[r,p] = corrcoef([strait.heat_flux strait.vol_flux strait.temperature]) ;
+heat_flux_correl.vol_flux.r    = r(1,2) ;
+heat_flux_correl.vol_flux.p    = p(1,2) ;
+heat_flux_correl.temperature.r = r(1,3) ;
+heat_flux_correl.temperature.p = p(1,3) ;
+
+[r,p] = corrcoef([strait.LFC_flux strait.vol_flux strait.salinity]) ;
+LFC_flux_correl.vol_flux.r = r(1,2) ;
+LFC_flux_correl.vol_flux.p = p(1,2) ;
+LFC_flux_correl.salinity.r = r(1,3) ;
+LFC_flux_correl.salinity.p = p(1,3) ;
+    
 end
