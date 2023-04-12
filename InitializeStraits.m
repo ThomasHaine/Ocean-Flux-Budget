40b10eca563901cb9124427fc7eb7e17e80ff56b
thaine1 <Thomas.Haine@jhu.edu>
2023-04-12 18:05:10 -0400

    Fix AR(1) process std

--- InitializeStraits.m
+++ InitializeStraits.m
@@ -166,11 +166,12 @@ sample_start = c / (1 - phi) + randn(1) * std / sqrt(1 - phi^2) ;
 
 % Compute
 v = zeros(N,1) ;
-x = sample_start ;         % Starting value
+x = sample_start ;                      % Starting value
+fac = sqrt(std^2 * (1 - phi^2)) ;       % See: https://en.wikipedia.org/wiki/Autoregressive_model#Example:_An_AR(1)_process
 if(N > 0)
     v(1) = x ;
     for ii=2:N
-        x = c + x * phi + randn(1) * std ;
+        x = c + x * phi + randn(1) * fac;
         v(ii) = x ;
     end % ii
 end % if
