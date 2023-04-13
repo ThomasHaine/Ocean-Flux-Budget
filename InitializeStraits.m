f199591b65fe944f12ea3d0f365e25347e7e20ed
thaine1 <Thomas.Haine@jhu.edu>
2023-04-13 08:51:50 -0400

    Update strait properties

--- InitializeStraits.m
+++ InitializeStraits.m
@@ -1,9 +1,11 @@
 function straits = InitializeStraits(DataParams,FluxParams)
 
-% Define cross-sectional area of each strait here. See Tsubouchi et al. (2018) Fig 1 for these rough estimates
-% Also define statistical properties of timeseries for each strait. 
-% See Tsubouchi et al. (2018) Table 3 and Figs. 2 and 4.
-% And Wang et al. Figure 2.
+% Define cross-sectional area of each strait here. See Tsubouchi et al.
+% (2018) Fig 1 for these rough estimates.
+% See also Tsubouchi et al. (2018) Table 3 and Figs. 2 and 4.
+% Also define statistical properties of timeseries for each strait. THese
+% are based on T_etal12 Table 3 and Hetal15 Fig. 4
+% See also Wang et al. Figure 2 for hydrographic statistics.
 % Compute mean speed from estimated volume flux and cross-sectional area.
 
 % West Fram Strait (EGC). This includes sea ice from Tetal12 Table 3.
@@ -77,15 +79,15 @@ DavisStrait = DefineStrait(DataParams,straitParams,"Davis Strait") ;
 % Bering Strait
 straitParams.area           = 3.8e6 ;       % Strait cross-sectional area [m^2]
 
-straitParams.speed_mean     = 1.0e6/straitParams.area ; % Speed average value [m/s]
-straitParams.speed_std      = (0.2/1.0)*straitParams.speed_mean ;      % Speed standard deviation [m/s]
-straitParams.speed_Delta    = 0.0 ;         % Speed change over timeseries [m/s]
+straitParams.speed_mean     = 1.0e6/straitParams.area ;                 % Speed average value [m/s]
+straitParams.speed_std      = (0.2/1.0)*straitParams.speed_mean ;       % Speed standard deviation [m/s]
+straitParams.speed_Delta    = 0.3e6/straitParams.area  ;                % Speed change over timeseries [m/s]
 
 straitParams.temp_mean      = 0.0 ;         % Temperature average value [C]
-straitParams.temp_std       = 0.5 ;         % Temperature standard deviation [C]
+straitParams.temp_std       = 0.25 ;        % Temperature standard deviation [C]
 straitParams.temp_Delta     = 1.0 ;         % Temperature change over timeseries [C]
 
-straitParams.salinity_mean  = 32.5 ;        % Salinity    average value [g/kg]
+straitParams.salinity_mean  = 32.4 ;        % Salinity    average value [g/kg]
 straitParams.salinity_std   = 0.1 ;         % Salinity    standard deviation [g/kg]
 straitParams.salinity_Delta = 0.0 ;         % Salinity    change over timeseries [g/kg]
 
@@ -96,7 +98,7 @@ BeringStrait = DefineStrait(DataParams,straitParams,"Bering Strait") ;
 straitParams.area           = 3.2e5 ;       % Strait cross-sectional area [m^2]
 
 straitParams.speed_mean     = 0.180e6/straitParams.area ; % Speed average value [m/s]
-straitParams.speed_std      = (0.018/0.18)*straitParams.speed_mean ;      % Speed standard deviation [m/s]
+straitParams.speed_std      = (0.009/0.18)*straitParams.speed_mean ;      % Speed standard deviation [m/s]
 straitParams.speed_Delta    = 0.0 ;         % Speed change over timeseries [m/s]
 
 straitParams.temp_mean      = 0.0 ;         % Temperature average value [C]
