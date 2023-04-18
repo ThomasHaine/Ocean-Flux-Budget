a4f7ff0192b7cc72c015ca9f509cd0f3187ab014
thaine1 <Thomas.Haine@jhu.edu>
2023-04-18 14:48:16 -0400

    Add CtrlVol data structure

--- InitializeDataParameters.m
+++ InitializeDataParameters.m
@@ -10,10 +10,10 @@ tmp.massBalance     = 'On' ;                    % Default flag for overall mass 
 tmp.C_p             = gsw_cp0 ;                 % Seawater specific heat capacity, J/kg/K
 
 % Initial control volume properties
-tmp.InitCtrlVolTemp = 0.0 ;                                             % Average starting temperature [C]
-tmp.InitCtrlVolSal  = 34.8 ;                                            % Average starting salinity [g/kg]
-tmp.InitCtrlVolDens = gsw_rho(tmp.InitCtrlVolSal,tmp.InitCtrlVolTemp,0) ;       % Average starting density [kg/m^3]
-tmp.InitCtrlVolVol  = (18.07 + 0.542 + 0.0028)*1e6*1e9 ;                % Starting volume [m^3]. From wiki for Arctic Ocean+Barents Sea+Baffin Bay
-tmp.InitCtrlVolMass = tmp.InitCtrlVolVol*tmp.InitCtrlVolDens ;          % Starting mass [kg]
-tmp.InitCtrlVolSalt = tmp.InitCtrlVolSal*tmp.InitCtrlVolMass*1e-3 ;     % Starting salt mass [kg]
+tmp.InitCtrlVolTemp = 0.0 ;                                                   % Average starting temperature [C]
+tmp.InitCtrlVolSal  = 34.8 ;                                                  % Average starting salinity [g/kg]
+tmp.InitCtrlVolDens = gsw_rho(tmp.InitCtrlVolSal,tmp.InitCtrlVolTemp,0) ;     % Average starting density [kg/m^3]
+tmp.InitCtrlVolVol  = (18.07 + 0.542 + 0.0028)*1e6*1e9 ;                      % Starting volume [m^3]. From wiki for Arctic Ocean+Barents Sea+Baffin Bay
+tmp.InitCtrlVolMass = tmp.InitCtrlVolVol*tmp.InitCtrlVolDens ;                % Starting mass [kg]
+tmp.InitCtrlVolSalt = tmp.InitCtrlVolSal*tmp.InitCtrlVolMass*1e-3 ;           % Starting salt mass [kg]
 end
