8e66cc6694d31f29e944764fadd030b1c7a92a4d
thaine1 <Thomas.Haine@jhu.edu>
2023-04-25 13:22:44 -0400

    Add component lines to CtrlVol T/S plots

--- ComputeCtrlVolProperties.m
+++ ComputeCtrlVolProperties.m
@@ -1,8 +1,8 @@
 function CtrlVol = ComputeCtrlVolProperties(budgets,DataParams)
-CtrlVol.mass   = DataParams.InitCtrlVolMass + budgets.mass.total ;
-CtrlVol.salt   = DataParams.InitCtrlVolSalt + budgets.salt.total ;
-CtrlVol.volume = DataParams.InitCtrlVolVol  + budgets.volume.total ;
-CtrlVol.sal    = (CtrlVol.salt.*1e3)./CtrlVol.mass ;
-CtrlVol.temp   = DataParams.InitCtrlVolTemp + budgets.heat.total./(CtrlVol.mass*DataParams.C_p) ;
-CtrlVol.dens   = gsw_rho(CtrlVol.sal,CtrlVol.temp,DataParams.p_ref) ;
+CtrlVol.mass        = DataParams.InitCtrlVolMass + budgets.mass.total ;
+CtrlVol.salt        = DataParams.InitCtrlVolSalt + budgets.salt.total ;
+CtrlVol.volume      = DataParams.InitCtrlVolVol  + budgets.volume.total ;
+CtrlVol.salinity    = (CtrlVol.salt.*1e3)./CtrlVol.mass ;
+CtrlVol.temperature = DataParams.InitCtrlVolTemp + budgets.heat.total./(CtrlVol.mass*DataParams.C_p) ;
+CtrlVol.density     = gsw_rho(CtrlVol.salinity,CtrlVol.temperature,DataParams.p_ref) ;
 end
