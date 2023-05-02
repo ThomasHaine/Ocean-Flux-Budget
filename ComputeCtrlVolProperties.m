0ec36b6b101275a82908995401dac67cff21d79c
thaine1 <Thomas.Haine@jhu.edu>
2023-05-02 17:39:21 -0400

    Tweak ComputeCtrlVolProperties.m for performance

--- ComputeCtrlVolProperties.m
+++ ComputeCtrlVolProperties.m
@@ -1,8 +1,10 @@
 function CtrlVol = ComputeCtrlVolProperties(budgets,DataParams)
 CtrlVol.mass        = DataParams.InitCtrlVolMass + budgets.mass.total ;
 CtrlVol.salt        = DataParams.InitCtrlVolSalt + budgets.salt.total ;
-CtrlVol.volume      = DataParams.InitCtrlVolVol  + budgets.volume.total ;
 CtrlVol.salinity    = (CtrlVol.salt.*1e3)./CtrlVol.mass ;
 CtrlVol.temperature = DataParams.InitCtrlVolTemp + budgets.heat.total./(CtrlVol.mass*DataParams.C_p) ;
-CtrlVol.density     = gsw_rho(CtrlVol.salinity,CtrlVol.temperature,DataParams.p_ref) ;
+
+% Currently, these variables aren't used:
+% CtrlVol.density     = gsw_rho(CtrlVol.salinity,CtrlVol.temperature,DataParams.p_ref) ;
+% CtrlVol.volume      = DataParams.InitCtrlVolVol  + budgets.volume.total ;
 end
